64fba843c576069725f6a85556ba4208163ea691
Paroxie <contessanguyen@gmail.com>
2024-11-12 18:44:35 -0600

    setting up the display proto background (trying to make a gradiant)

---
+++ Final.py
@@ -0,0 +1,15 @@
+import pygame 
+import random
+
+
+pygame.init()
+
+
+# Display (test background)
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Final Project")
+
+Pink = (255, 182, 193)
+Blue = (173, 216, 230)
+White = (255, 255, 255)
