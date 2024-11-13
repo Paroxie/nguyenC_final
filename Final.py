e2a917e862c0c488bb410b048a6fcce526c60ba6
Paroxie <contessanguyen@gmail.com>
2024-11-12 18:47:01 -0600

    adding spakles and setting up to make them fade in and out

--- Final.py
+++ Final.py
@@ -13,3 +13,16 @@ pygame.display.set_caption("Final Project")
 Pink = (255, 182, 193)
 Blue = (173, 216, 230)
 White = (255, 255, 255)
+
+
+class sparkles:
+    def __init__(self, x, y, size): 
+        self.x = x
+        self.y = y
+        self.size = size
+        self.alpha = random.randint(100, 255)  #
+        self.alpha_direction = random.choice([-1, 1])  # Fade in or out
+        self.max_alpha = 255
+        self.min_alpha = 50
+
+    def update(self):
